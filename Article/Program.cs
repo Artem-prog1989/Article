092b8b654b5ad6e4003d234328800667d1015b68
кабася <@HOMEPC>
2022-09-18 14:21:33 +0300

    Добавьте файлы проекта.

---
+++ Article/Program.cs
@@ -0,0 +1,61 @@
+﻿class Article
+{
+    string name;
+    string shop;
+    int price;
+    public string Name  
+    {
+        get { return name; } 
+    }
+    public string Shop
+    {
+        get { return shop; }
+    }
+    
+    public int Price
+    {
+        get { return price; }
+    }
+    public Article(string name, string shop, int price)
+    {
+        this.name = name;
+        this.shop = shop;
+        this.price = price;
+    }
+}
+
+class Store
+{
+    Article[] articles;
+    public Store(Article[] article)
+    {
+        this.articles = article;
+    }
+
+    public Article this[int index]
+    {
+        
+        get
+        {
+            if(index<articles.Length|| index > 0)
+            {
+                return articles[index];
+            }
+            else
+            {
+                throw new IndexOutOfRangeException();
+            }
+            
+        }
+        
+       
+    }
+}
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+        Store store = new Store(new Article[] { new Article("телевизор", "Зодиак", 300), new Article("холодильник", "Норд", 500), new Article("шкаф", "мир мебели", 700) });
+        Console.WriteLine(store[2].Name);
+        }
+    }
